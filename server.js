fb0aa877cf9538899452dfae71010433400e9f0f
Tarek Elkurjie <tarekelkurjie@gmail.com>
2021-11-23 11:56:05 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './src/index.html'))
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'));
